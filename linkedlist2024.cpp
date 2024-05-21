8cae858feaa5ab0bc31ab5e1bebbbd42b61e71fa
RayCarro <raycarro2004@yahoo.com>
2024-05-21 07:55:14 -0500

    initial commit

---
+++ linkedlist2024.cpp
@@ -0,0 +1,128 @@
+﻿// linkedlist2024.cpp : Defines the entry point for the application.
+//
+
+#include "linkedlist2024.h"
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <initializer_list>
+
+using namespace std;
+using namespace LinkedList2024;
+
+// wasn't sure if 3rd party libraries are allowed, so I didn't want to use a testing library
+template<class DataType>
+void test_list(const LinkedList<DataType>& list, const initializer_list<DataType>& expected) {
+	int i = 0;
+	auto e = expected.begin();
+
+	cout << endl << "testing list..." << endl;
+
+	for (const auto& n : list) {
+		assert(e != expected.end());
+		cout << "#" << i << ": " << n << " vs " << *e << endl;
+		assert(n == *e);
+		++e;
+		i++;
+	}
+
+	assert(e == expected.end());
+}
+
+class TestMemoryLeaks
+{
+public:
+	static int contructs, destructs;
+
+	TestMemoryLeaks() { contructs++; }
+	TestMemoryLeaks(const TestMemoryLeaks& other) { contructs++; }
+	~TestMemoryLeaks() { destructs++; }
+};
+int TestMemoryLeaks::contructs = 0;
+int TestMemoryLeaks::destructs = 0;
+
+int main()
+{
+	int i;
+
+	// test ints
+	LinkedList<int> list({ 0,1,2,3,4 });
+	test_list(list, { 0,1,2,3,4 });
+
+	// test crawling backwards
+	i = 4;
+	for (auto n = list.rend(); n != list.rbegin(); ++n) {
+		assert(*n == i--);
+	}
+
+	// remove odd numbers
+	for (auto n = list.begin(); n != list.end();) {
+		if (*n % 2 == 1) {
+			n = list.erase(n);
+		}
+		else {
+			++n;
+		}
+	}
+
+	test_list(list, { 0,2,4 });
+
+	// test clear and push new ints
+	list.clear();
+
+	list.push_back(0);
+	list.push_back(4);
+	list.push_back(5);
+	list.push_back(1);
+
+	test_list(list, { 0,4,5,1 });
+
+	// test find_first
+	assert(*list.find_first(5) == 5);
+	assert(*--list.find_first(5) == 4);
+	assert(*++list.find_first(5) == 1);
+
+	// test pop_front and pop_back
+	list.pop_front();
+	test_list(list, { 4,5,1 });
+	list.pop_back();
+	test_list(list, { 4,5 });
+
+	// test implicit clear and re-init
+	list = { 3,2,1 };
+	test_list(list, { 3,2,1 });
+
+	// test list of strings
+	LinkedList<string> slist;
+
+	slist.push_front("a");
+	slist.push_front("b");
+	slist.push_front("c");
+	slist.push_front("d");
+	test_list<string>(slist, { "d", "c", "b", "a"});
+
+	// test erasing individual elements, and find_first
+	slist.erase(slist.find_first("c"));
+	test_list<string>(slist, { "d", "b", "a" });
+
+	slist.erase(slist.find_first("b"));
+	test_list<string>(slist, { "d", "a" });
+
+	slist.erase(slist.find_first("d"));
+	test_list<string>(slist, { "a" });
+
+	slist.erase(slist.find_first("a"));
+	test_list<string>(slist, { });
+
+	// test for memory leaks
+	LinkedList<TestMemoryLeaks> test_mem_leaks_list;
+	test_mem_leaks_list.push_back(TestMemoryLeaks());
+	test_mem_leaks_list.push_back(TestMemoryLeaks());
+	test_mem_leaks_list.push_back(TestMemoryLeaks());
+	test_mem_leaks_list.clear();
+	assert(TestMemoryLeaks::contructs == 6);
+	assert(TestMemoryLeaks::destructs == 6);
+
+	cout << endl << "success!" << endl;
+	return 0;
+}
---
+++ linkedlist2024.h
@@ -0,0 +1,248 @@
+﻿#pragma once
+#include <assert.h>
+#include <initializer_list>
+#include <utility>
+
+namespace LinkedList2024 {
+	template<class DataType>
+	class LinkedList;
+	template<class DataType>
+	class Iterator;
+
+	template<class DataType>
+	class Node
+	{
+		friend class LinkedList<DataType>;
+		friend class Iterator<DataType>;
+
+		Node *_prev, *_next;
+		DataType _d;
+
+		Node(DataType&& value)
+			: _d(std::move(value)) {
+			_prev = nullptr;
+			_next = nullptr;
+		}
+
+		Node(const DataType& value)
+			: _d(value) {
+			_prev = nullptr;
+			_next = nullptr;
+		}
+	};
+
+	template<class DataType>
+	class Iterator
+	{
+		friend class LinkedList<DataType>;
+
+		Node<DataType>* _ptr;
+		bool _reverse;
+
+		Iterator(Node<DataType>* newPtr, bool reverseDirection=false) {
+			_ptr = newPtr;
+			_reverse = reverseDirection;
+		}
+
+
+	public:
+		Iterator<DataType>& operator++() {
+			if (_reverse) {
+				_ptr = _ptr->_prev;
+			}
+			else {
+				_ptr = _ptr->_next;
+			}
+			return *this;
+		}
+
+		Iterator<DataType>& operator--() {
+			if (_reverse) {
+				_ptr = _ptr->_next;
+			}
+			else {
+				_ptr = _ptr->_prev;
+			}
+			return *this;
+		}
+
+		bool operator!=(const Iterator &other) const {
+			return _ptr != other._ptr;
+		}
+
+		DataType& operator*() const {
+			return _ptr->_d;
+		}
+	};
+
+	template<class DataType>
+	class LinkedList
+	{
+	public:
+		~LinkedList() {
+			clear();
+		}
+
+		LinkedList() {
+			_first = nullptr;
+			_last = nullptr;
+		}
+
+		LinkedList(LinkedList<DataType>&& other) {
+			_first = other._first;
+			_last = other._last;
+			other._first = nullptr;
+			other._last = nullptr;
+		}
+
+		LinkedList(const std::initializer_list<DataType>& init) {
+			_first = nullptr;
+			_last = nullptr;
+
+			operator=(init);
+		}
+
+		auto& operator=(const std::initializer_list<DataType>& init) {
+			clear();
+			for (auto& v : init) {
+				push_back(v);
+			}
+			return *this;
+		}
+
+		auto insert(Iterator<DataType> it, DataType&& data) {
+			auto* n = new Node<DataType>(std::move(data));
+			return _insert(it, n);
+		}
+
+		auto insert(Iterator<DataType> it, const DataType& data) {
+			auto* n = new Node<DataType>(data);
+			return _insert(it, n);
+		}
+
+		auto push_front(DataType&& data) {
+			return insert(rbegin(), std::move(data));
+		}
+
+		auto push_front(const DataType& data) {
+			return insert(rbegin(), data);
+		}
+
+		auto push_back(DataType&& data) {
+			return insert(end(), std::move(data));
+		}
+
+		auto push_back(const DataType& data) {
+			return insert(end(), data);
+		}
+
+		void clear() {
+			while(_first != nullptr) {
+				auto p = _first;
+				_first = p->_next;
+				delete p;
+			}
+			_last = nullptr;
+		}
+
+		auto erase(Iterator<DataType> it) {
+			Node<DataType>* n = it._ptr;
+			assert(n != nullptr);
+			++it;
+			auto prev = n->_prev;
+			auto next = n->_next;
+
+			if (_first == n) {
+				_first = next;
+			}
+			if (_last == n) {
+				_last = prev;
+			}
+			if (prev != nullptr) {
+				prev->_next = next;
+			}
+			if (next != nullptr) {
+				next->_prev = prev;
+			}
+
+			delete n;
+			return it;
+		}
+
+		Iterator<DataType> find_first(const DataType& val) const {
+			for (Iterator<DataType> it = begin(); it != end(); ++it) {
+				if (*it == val) {
+					return it;
+				}
+			}
+			return end();
+		}
+
+		auto pop_front() {
+			return erase(begin());
+		}
+
+		auto pop_back() {
+			return erase(rend());
+		}
+
+		Iterator<DataType> begin() const {
+			return Iterator<DataType>(_first);
+		}
+
+		Iterator<DataType> end() const {
+			return Iterator<DataType>(nullptr);
+		}
+
+		Iterator<DataType> rbegin() const {
+			return Iterator<DataType>(nullptr, true);
+		}
+
+		Iterator<DataType> rend() const {
+			return Iterator<DataType>(_last, true);
+		}
+
+    private:
+        Node<DataType>* _first, * _last;
+
+        auto _insert(const Iterator<DataType>& it, Node<DataType>* n) {
+            assert(n != nullptr);
+
+            auto ptr = it._ptr;
+
+            if (it._reverse && ptr == nullptr) {
+                n->_prev = nullptr;
+                n->_next = _first;
+            }
+            else if (it._reverse && ptr != nullptr) {
+                n->_prev = ptr->_prev;
+                n->_next = ptr;
+            }
+            else if (!it._reverse && ptr != nullptr) {
+                n->_prev = ptr;
+                n->_next = ptr->_next;
+            }
+            else {
+                // ptr is null, this is for push_back
+                n->_prev = _last;
+                n->_next = nullptr;
+            }
+
+            if (n->_prev == nullptr) {
+                _first = n;
+                _last = n;
+            }
+            else if (n->_prev == _last) {
+                _last = n;
+            }
+            if (n->_prev != nullptr) {
+                n->_prev->_next = n;
+            }
+            if (n->_next != nullptr) {
+                n->_next->_prev = n;
+            }
+            return Iterator<DataType>(n);
+        }
+	};
+
+};
